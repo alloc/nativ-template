666ac2b3f618a98f84df079df02c931eb31a9db8
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-20 23:48:25 -0400

    feat: set android.edgeToEdgeEnabled=true

--- app.config.ts
+++ app.config.ts
@@ -18,6 +18,7 @@ const expo: ExpoConfig = {
   },
   android: {
     package: 'com.example.mynativapp',
+    edgeToEdgeEnabled: true,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
