69b4cbff27a1db10ca95caf6ccbcd009007bb698
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-20 22:23:36 -0400

    feat: use app.config.ts instead of app.json
    …and set up expo-splash-screen.

---
+++ app.config.ts
@@ -0,0 +1,64 @@
+import { ExpoConfig } from 'expo/config'
+
+const expo: ExpoConfig = {
+  name: 'My Nativ App',
+  slug: 'my-nativ-app',
+  version: '1.0.0',
+  orientation: 'portrait',
+  scheme: 'my-nativ-app',
+  userInterfaceStyle: 'light',
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.example.mynativapp',
+    icon: {
+      dark: './assets/images/icon-dark.png',
+      light: './assets/images/icon-light.png',
+      tinted: './assets/images/icon-tinted.png',
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    'expo-font',
+    'expo-sqlite',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          newArchEnabled: true,
+        },
+        android: {
+          newArchEnabled: true,
+        },
+      },
+    ],
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash.png',
+        imageWidth: 200,
+        backgroundColor: '#232323',
+        dark: {
+          image: './assets/images/splash-dark.png',
+          backgroundColor: '#000000',
+        },
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+}
+
+export default expo
--- assets/images/README.md
+++ assets/images/README.md
@@ -4,9 +4,12 @@ This folder contains the image assets for your app.
 
 ## Required files:
 
-- `icon.png` - App icon (1024x1024)
+- `icon.png` - iOS app icon (1024x1024)
+- `icon-dark.png` - iOS app icon (1024x1024)
+- `icon-tinted.png` - iOS app icon (1024x1024)
 - `adaptive-icon.png` - Android adaptive icon foreground (1024x1024)
 - `splash.png` - Splash screen image
+- `splash-dark.png` – Splash screen image
 - `favicon.png` - Web favicon (48x48)
 
 You can replace these placeholder files with your actual app assets.
