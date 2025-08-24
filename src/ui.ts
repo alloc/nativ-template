082ea9d0d2c85fd05d49b02aae92f208f6ae328a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-24 00:21:16 -0400

    feat: upgrade to latest nativ API

--- src/ui.ts
+++ src/ui.ts
@@ -1,14 +1,22 @@
-import { createPrimitives } from 'nativ/ui'
+import {
+  createImage,
+  createMotiImage,
+  createMotiScrollView,
+  createMotiText,
+  createMotiView,
+  createPressable,
+  createScrollView,
+  createText,
+  createView,
+} from 'nativ/ui'
 import type { Theme } from './theme'
 
-export const {
-  View,
-  Text,
-  MotiView,
-  MotiImage,
-  MotiScrollView,
-  MotiText,
-  Pressable,
-  ScrollView,
-  Image,
-} = createPrimitives<Theme>()
+export const View = createView<Theme>()
+export const Text = createText<Theme>()
+export const MotiView = createMotiView<Theme>()
+export const MotiImage = createMotiImage<Theme>()
+export const MotiScrollView = createMotiScrollView<Theme>()
+export const MotiText = createMotiText<Theme>()
+export const Pressable = createPressable<Theme>()
+export const ScrollView = createScrollView<Theme>()
+export const Image = createImage<Theme>()
