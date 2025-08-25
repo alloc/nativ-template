0a029993555100aa3cc7b4d11a9416a7354e85f7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-25 18:48:20 -0400

    feat(template): add Suspense boundary to AppLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,14 @@ import 'nativ/polyfills'
 
 import { SplashScreen, Stack } from 'expo-router'
 import { AppProvider } from 'nativ/providers'
+import { ActivityIndicator } from 'react-native'
 
+import { Suspense } from 'react'
 import { db } from '~/db/client'
 import migrations from '~/db/migrations'
 import { queryClient } from '~/queryClient'
 import { theme } from '~/theme'
+import { View } from '~/ui'
 
 const fonts = {
   InterTight: require('~/fonts/InterTight[wght].ttf'),
@@ -24,12 +27,22 @@ export default function AppLayout() {
       theme={theme}
       queryClient={queryClient}
       onLoad={() => SplashScreen.hideAsync()}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <Suspense fallback={<SuspenseFallback />}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </Suspense>
     </AppProvider>
+  )
+}
+
+function SuspenseFallback() {
+  return (
+    <View flex={1} justifyContent="center" alignItems="center">
+      <ActivityIndicator size="small" />
+    </View>
   )
 }
 
