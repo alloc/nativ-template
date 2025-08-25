a773f03c9e4e767752543f7fc132095d000e7aae
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-25 17:07:15 -0400

    feat(template): update the Drizzle client to use V2 relations

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,10 +1,13 @@
 import { drizzle } from 'drizzle-orm/expo-sqlite'
 import { openDatabaseSync } from 'expo-sqlite'
+import { relations } from './relations'
 import * as schema from './schema'
-
-const sql = openDatabaseSync('app.db')
 
 /**
  * Local database powered by expo-sqlite.
  */
-export const db = drizzle(sql, { schema })
+export const db = drizzle(openDatabaseSync('app.db'), {
+  schema,
+  relations,
+  casing: 'snake_case',
+})
